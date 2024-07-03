cbb3d5fc73fb7be6e43b37bdb8ca112e11ea6287
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-07-03 22:23:01 +0100

    Add excel to csv functionality

--- app.ts
+++ app.ts
@@ -10,6 +10,7 @@ import xlsx from 'xlsx';
 import os from 'os';
 import path from 'path';
 import fs from 'fs';
+import AdmZip from 'adm-zip';
 
 //#region app setup
 const app = express();
@@ -45,6 +46,26 @@ const SWAGGER_OPTIONS = {
         name: 'Default',
         description: 'Default API Operations that come inbuilt',
       },
+      {
+        name: 'Excel',
+        description: 'Excel file conversions',
+      },
+      {
+        name: 'CSV',
+        description: 'CSV file conversions',
+      },
+      {
+        name: 'JSON',
+        description: 'JSON file conversions',
+      },
+      {
+        name: 'SQL',
+        description: 'SQL file conversions',
+      },
+      {
+        name: 'XML',
+        description: 'XML file conversions',
+      },
     ],
   },
   apis: ['**/*.ts'], // Define the paths to your API routes
@@ -72,6 +93,8 @@ const PORT = process.env.PORT || 3000;
 const baseURL = 'https://httpbin.org';
 //#endregion
 
+//#region code here
+
 /**
  * @swagger
  * /excel-json:
@@ -97,59 +120,140 @@ const baseURL = 'https://httpbin.org';
  *       '400':
  *         description: Bad request
  */
-app.post('/excel-json', upload.single('file'), (req: Request, res: Response) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
+app.post(
+  '/excel-json',
+  upload.single('file'),
+  (req: Request, res: Response) => {
+    if (!req.file) return res.status(400).send('No file uploaded.');
+
+    const filePath = req.file.path;
+    const workbook = xlsx.readFile(filePath);
+    const sheetNames = workbook.SheetNames;
+    const jsonResult: { [key: string]: any[] } = {};
+
+    sheetNames.forEach((sheetName) => {
+      const worksheet = workbook.Sheets[sheetName];
+      jsonResult[sheetName] = xlsx.utils.sheet_to_json(worksheet);
+    });
+
+    const tempDir = os.tmpdir();
+
+    // Ensure the directory exists
+    // const jsonDir = path.join(__dirname, 'uploads');
+    // if (!fs.existsSync(jsonDir)) fs.mkdirSync(jsonDir);
+
+    const jsonFilePath = path.join(tempDir, `${req.file.filename}.json`);
+
+    fs.writeFile(
+      jsonFilePath,
+      JSON.stringify(jsonResult, null, 2),
+      (writeErr) => {
+        if (writeErr) {
+          return res.status(500).send('Error creating JSON file.');
+        }
+
+        // Set the file download headers and send the file
+        res.setHeader(
+          'Content-disposition',
+          `attachment; filename=${req.file?.originalname}.json`
+        );
+        res.setHeader('Content-type', 'application/json');
+        res.sendFile(jsonFilePath, (err) => {
+          if (err) {
+            res.status(500).send('Error downloading the file.');
+          } else {
+            // Optional: clean up the uploaded Excel and JSON files
+            // fs.unlink(filePath, (unlinkErr) => {
+            //   if (unlinkErr) console.error(`Error deleting file ${filePath}`);
+            // });
+            // fs.unlink(jsonFilePath, (unlinkErr) => {
+            //   if (unlinkErr) console.error(`Error deleting file ${jsonFilePath}`);
+            // });
+          }
+        });
+      }
+    );
   }
+);
+
+/**
+ * @swagger
+ * /excel-csv:
+ *   post:
+ *     summary: Upload an excel file to be converted to csv
+ *     description: This will return a csv file
+ *     tags:
+ *       - Excel
+ *     requestBody:
+ *       description: Excel file to be converted
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       '200':
+ *         description: Successfully created a new document
+ *       '400':
+ *         description: Bad request
+ */
+app.post('/excel-csv', upload.single('file'), (req: Request, res: Response) => {
+  if (!req.file) return res.status(400).send('No file uploaded.');
 
   const filePath = req.file.path;
+  const fileName = req.file.filename;
   const workbook = xlsx.readFile(filePath);
   const sheetNames = workbook.SheetNames;
-  const jsonResult: { [key: string]: any[] } = {};
+  const tempDir = os.tmpdir();
+  const csvFiles: string[] = [];
 
   sheetNames.forEach((sheetName) => {
     const worksheet = workbook.Sheets[sheetName];
-    jsonResult[sheetName] = xlsx.utils.sheet_to_json(worksheet);
+    const csvData = xlsx.utils.sheet_to_csv(worksheet);
+    const csvFilePath = path.join(tempDir, `${fileName}-${sheetName}.csv`);
+
+    fs.writeFileSync(csvFilePath, csvData);
+    csvFiles.push(csvFilePath);
   });
 
-  const tempDir = os.tmpdir();
+  const zip = new AdmZip();
+  csvFiles.forEach((csvFile) => {
+    zip.addLocalFile(csvFile);
+  });
 
-  // Ensure the directory exists
-  // const jsonDir = path.join(__dirname, 'uploads');
-  // if (!fs.existsSync(jsonDir)) fs.mkdirSync(jsonDir);
+  const zipFilePath = path.join(tempDir, `${req.file.filename}.zip`);
+  zip.writeZip(zipFilePath);
 
-  const jsonFilePath = path.join(tempDir, `${req.file.filename}.json`);
-
-  fs.writeFile(
-    jsonFilePath,
-    JSON.stringify(jsonResult, null, 2),
-    (writeErr) => {
-      if (writeErr) {
-        return res.status(500).send('Error creating JSON file.');
-      }
-
-      // Set the file download headers and send the file
-      res.setHeader(
-        'Content-disposition',
-        `attachment; filename=${req.file?.originalname}.json`
-      );
-      res.setHeader('Content-type', 'application/json');
-      res.sendFile(jsonFilePath, (err) => {
-        if (err) {
-          res.status(500).send('Error downloading the file.');
-        } else {
-          // Optional: clean up the uploaded Excel and JSON files
-          // fs.unlink(filePath, (unlinkErr) => {
-          //   if (unlinkErr) console.error(`Error deleting file ${filePath}`);
-          // });
-          // fs.unlink(jsonFilePath, (unlinkErr) => {
-          //   if (unlinkErr) console.error(`Error deleting file ${jsonFilePath}`);
-          // });
-        }
-      });
-    }
+  res.setHeader(
+    'Content-disposition',
+    `attachment; filename=${req.file?.originalname}.zip`
   );
+  res.setHeader('Content-type', 'application/zip');
+  res.sendFile(zipFilePath, (err) => {
+    if (err) {
+      res.status(500).send('Error downloading the file.');
+    } else {
+      // Optional: clean up the uploaded Excel and CSV files
+      // fs.unlink(filePath, (unlinkErr) => {
+      //   if (unlinkErr) console.error(`Error deleting file ${filePath}`);
+      // });
+      // csvFiles.forEach((csvFile) => {
+      //   fs.unlink(csvFile, (unlinkErr) => {
+      //     if (unlinkErr) console.error(`Error deleting file ${csvFile}`);
+      //   });
+      // });
+      // fs.unlink(zipFilePath, (unlinkErr) => {
+      //   if (unlinkErr) console.error(`Error deleting file ${zipFilePath}`);
+      // });
+    }
+  });
 });
+
+//#endregion
 
 //#region Server setup
 async function pingSelf() {

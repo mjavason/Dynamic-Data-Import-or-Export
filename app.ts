2577ca7c892482deefcae80e468faa0a1672db93
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-07-03 21:23:54 +0100

    Finished excel to json conversion

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ import 'express-async-errors';
 import cors from 'cors';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import multer from 'multer';
 import xlsx from 'xlsx';
 import os from 'os';
@@ -11,9 +13,47 @@ import fs from 'fs';
 
 //#region app setup
 const app = express();
+const SWAGGER_OPTIONS = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Dynamic Data Export/Import App',
+      version: '1.0.0',
+      description:
+        'Think of this as a data converter. Want to export your mongoDB database as an excel sheet? No problem. Want to Import from a SQL database to CSV or JSON? No problem.',
+      contact: {
+        name: 'Orji Michael',
+        email: 'orjimichael4886@gmail.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000', // Development environment
+        description: 'Development Environment',
+      },
+      {
+        url: 'https://live.onrender.com/api/v1', // Staging environment
+        description: 'Staging Environment',
+      },
+      // {
+      //   url: 'https://api.example.com/api/v1', // Production environment
+      //   description: 'Production Environment',
+      // },
+    ],
+    tags: [
+      {
+        name: 'Default',
+        description: 'Default API Operations that come inbuilt',
+      },
+    ],
+  },
+  apis: ['**/*.ts'], // Define the paths to your API routes
+};
+const swaggerSpec = swaggerJSDoc(SWAGGER_OPTIONS);
 app.use(express.json()); // Middleware to parse JSON or URL-encoded data
 app.use(express.urlencoded({ extended: true })); // For complex form data
 app.use(cors());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 dotenv.config({ path: './.env' });
 const tempDir = os.tmpdir(); // Get the system temporary directory
 const storage = multer.diskStorage({
@@ -32,7 +72,32 @@ const PORT = process.env.PORT || 3000;
 const baseURL = 'https://httpbin.org';
 //#endregion
 
-app.get('/excel-json', upload.single('file'), (req: Request, res: Response) => {
+/**
+ * @swagger
+ * /excel-json:
+ *   post:
+ *     summary: Upload an excel file to be converted to json
+ *     description: This will return a json file
+ *     tags:
+ *       - Excel
+ *     requestBody:
+ *       description: Excel file to be converted
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       '200':
+ *         description: Successfully created a new document
+ *       '400':
+ *         description: Bad request
+ */
+app.post('/excel-json', upload.single('file'), (req: Request, res: Response) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
@@ -47,14 +112,13 @@ app.get('/excel-json', upload.single('file'), (req: Request, res: Response) => {
     jsonResult[sheetName] = xlsx.utils.sheet_to_json(worksheet);
   });
 
-  // Create a JSON file from the result
-  const jsonDir = path.join(__dirname, 'uploads');
-  const jsonFilePath = path.join(jsonDir, `${req.file.filename}.json`);
+  const tempDir = os.tmpdir();
 
   // Ensure the directory exists
-  if (!fs.existsSync(jsonDir)) {
-    fs.mkdirSync(jsonDir);
-  }
+  // const jsonDir = path.join(__dirname, 'uploads');
+  // if (!fs.existsSync(jsonDir)) fs.mkdirSync(jsonDir);
+
+  const jsonFilePath = path.join(tempDir, `${req.file.filename}.json`);
 
   fs.writeFile(
     jsonFilePath,
@@ -88,7 +152,32 @@ app.get('/excel-json', upload.single('file'), (req: Request, res: Response) => {
 });
 
 //#region Server setup
+async function pingSelf() {
+  try {
+    const { data } = await axios.get(`http://localhost:5000`);
+
+    console.log(`Server pinged successfully: ${data.message}`);
+    return true;
+  } catch (e: any) {
+    console.log(`this the error message: ${e.message}`);
+    return;
+  }
+}
+
 // default message
+/**
+ * @swagger
+ * /api:
+ *   get:
+ *     summary: Call a demo extenal API (httpbin.org)
+ *     description: Returns an object containing demo content
+ *     tags: [Default]
+ *     responses:
+ *       '200':
+ *         description: Successful.
+ *       '400':
+ *         description: Bad request.
+ */
 app.get('/api', async (req: Request, res: Response) => {
   const result = await axios.get(baseURL);
   console.log(result.status);
@@ -99,19 +188,52 @@ app.get('/api', async (req: Request, res: Response) => {
 });
 
 //default message
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: API Health check
+ *     description: Returns an object containing demo content
+ *     tags: [Default]
+ *     responses:
+ *       '200':
+ *         description: Successful.
+ *       '400':
+ *         description: Bad request.
+ */
 app.get('/', (req: Request, res: Response) => {
   return res.send({ message: 'API is Live!' });
+});
+
+// Middleware to handle 404 Not Found
+/**
+ * @swagger
+ * /obviously/this/route/cant/exist:
+ *   get:
+ *     summary: API 404 Response
+ *     description: Returns a non crashing result when you try to run a route that doesnt exist
+ *     tags: [Default]
+ *     responses:
+ *       '404':
+ *         description: Route not found
+ */
+app.use((req: Request, res: Response) => {
+  return res
+    .status(404)
+    .json({ success: false, message: 'API route does not exist' });
 });
 
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+// (for render services) Keep the API awake by pinging it periodically
+// setInterval(pingSelf, 600000);
+
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   // throw Error('This is a sample error');
 
   console.log(`${'\x1b[31m'}${err.message}${'\x1b][0m]'}`);
-  console.log('---');
   return res
     .status(500)
     .send({ success: false, status: 500, message: err.message });
